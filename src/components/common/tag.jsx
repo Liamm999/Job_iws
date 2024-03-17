ad7cf72a73525e7f377b98addc38c5b53be0f84e
Lê Lâm <89892749+Liamm999@users.noreply.github.com>
2024-03-17 21:58:51 +0700

    Delete src/components directory

--- src/components/common/tag.jsx
+++
@@ -1,9 +0,0 @@
-export const Tag = ({ children, className }) => {
-  return (
-    <div
-      className={`bg-black/[.8] px-7 py-4 rounded-2xl w-full h-max ${className}`}
-    >
-      {children}
-    </div>
-  );
-};
